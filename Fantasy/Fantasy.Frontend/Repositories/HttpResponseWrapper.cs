72e1e6bdf40401113171b00b30b7c249efd12de4
Emilio Barrera <barrera_emilio@yahoo.es>
2024-08-24 13:12:39 +0100

    Creating the repository on the fontend

---
+++ Fantasy/Fantasy.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+
+namespace Fantasy.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statuCode = HttpResponseMessage.StatusCode;
+            if (statuCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado.";
+            }
+
+            if (statuCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+
+            if (statuCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que estar logeado para ejecutar esta operación.";
+            }
+
+            if (statuCode == HttpStatusCode.Forbidden)
+            {
+                return "No tienes permisos para hacer esta operación.";
+            }
+
+            return "Ha ocurrido un error inesperado.";
+        }
+    }
+}
